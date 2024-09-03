f9360ff28fe9f452018d5ed9fa06d7f9ac3c0d22
Stephen Barreto <stp.sip7@gmail.com>
2024-09-02 20:31:37 -0500

    svelte component and react installation

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    let greeting;
+
+    const getGreeting = () => {
+        const hour = new Date().getHours();
+        if (hour < 12) {
+            return "Buenos días ☀️";
+        } else if (hour < 18) {
+            return "Buenas tardes 🌤️";
+        } else {
+            return "Buenas noches 🌙";
+        }
+    };
+
+    greeting = getGreeting();
+</script>
+
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
